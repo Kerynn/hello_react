57ec904f7752a74d9a4b0347f9c15a365bc83199
Kerynn Davis <kerynn.davis@gmail.com>
2023-05-24 15:28:44 -0700

    use state to set roles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       { console.log('inside the return') }
       { showEmployees ? (
         <>
+          <input 
+            type="text" 
+            onChange={(e) => {
+              console.log(e.target.value)
+              setRole(e.target.value)
+            }}
+          />
           <Employee name="Charlie" role="Intern"/>
-          <Employee name="Jim"/>
+          <Employee name="Jim" role={role} />
           <Employee name="Kamee"/>
         </>
       ) : (
