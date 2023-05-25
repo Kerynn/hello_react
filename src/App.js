a4ac1227d8f23cc4528e7bbf23ab1636657ac00b
Kerynn Davis <kerynn.davis@gmail.com>
2023-05-25 11:43:34 -0700

    add flex wrap to employees

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       { console.log('inside the return') }
       { showEmployees ? (
         <>
@@ -17,9 +17,15 @@ function App() {
               setRole(e.target.value)
             }}
           />
-          <Employee name="Charlie" role="Intern"/>
-          <Employee name="Jim" role={role} />
-          <Employee name="Kamee"/>
+            <div className="flex flex-wrap">
+              <Employee name="Charlie" role="Intern"/>
+              <Employee name="Jim" role={role} />
+              <Employee name="Kamee"/>
+              <Employee name="Francis" role="Software Engineer"/>
+              <Employee name="Nicole" role={role} />
+              <Employee name="Sammy" role={role}/>
+              <Employee name="Ozzie"/>
+            </div>
         </>
       ) : (
         <p>You cannot see the employees</p>
