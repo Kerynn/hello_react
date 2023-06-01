9ae7e5616ea389e5e337394df3e6c4e07028ef0c
Kerynn Davis <kerynn.davis@gmail.com>
2023-06-01 11:58:06 -0700

    add update employee function

--- src/App.js
+++ src/App.js
@@ -7,41 +7,59 @@ function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState([
       {
+        id: 1,
         name: "Charlie", 
         role: "Intern", 
         img: "https://images.pexels.com/photos/8121155/pexels-photo-8121155.jpeg"
       },
       {
+        id: 2,
         name: "Jim",
         role: role, 
         img: "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg"
       },
       {
+        id: 3,
         name: "Kamee",
         role: "Team Manager",
         img: "https://images.pexels.com/photos/800330/pexels-photo-800330.jpeg"
       },
       {
+        id: 4,
         name: "Francis",
         role: "Software Engineer",
         img: "https://images.pexels.com/photos/3658120/pexels-photo-3658120.jpeg"
       },
       {
+        id: 5,
         name: "Nicole",
         role: "Marketing Director",
         img: "https://images.pexels.com/photos/2463238/pexels-photo-2463238.jpeg"
       },
       {
+        id: 6,
         name: "Sammy",
         role: role,
         img: "https://images.pexels.com/photos/4588052/pexels-photo-4588052.jpeg"
       },
       {
+        id: 7,
         name: "Ozzie",
         role: "Security",
         img: "https://images.pexels.com/photos/2774140/pexels-photo-2774140.jpeg"
       }
     ]);
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id){
+        return {...employee, name: newName, role: newRole}
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees)
+  }
+
   const showEmployees = true;
   return (
     <div className="App bg-blue-200">
@@ -51,7 +69,6 @@ function App() {
           <input 
             type="text" 
             onChange={(e) => {
-              console.log(e.target.value)
               setRole(e.target.value)
             }}
           />
@@ -60,11 +77,13 @@ function App() {
               console.log(employee);
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
                   alt="Employee Photo"
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
