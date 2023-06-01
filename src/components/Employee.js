b865022110b0ce890a31a0b2ac6caf38274ac70c
Kerynn Davis <kerynn.davis@gmail.com>
2023-06-01 11:00:34 -0700

    update bootstrap button with update employee info button

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,3 +1,5 @@
+import EditEmployee from './EditEmployee';
+
 function Employee(props){
     return (
       <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
@@ -10,9 +12,9 @@ function Employee(props){
             <p className="text-lg font-semibold text-black">{props.name}</p>
             <p className="text-slate-600 font-medium">{props.role}</p>
           </div>
-          <button className="px-4 py-1 text-sm text-blue-800 font-semibold rounded-full border border-blue-900 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-            Update Employee Info
-          </button>
+
+          <EditEmployee />
+
         </div>
       </div>
     )
