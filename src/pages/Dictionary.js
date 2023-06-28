0871e7a93cedb550c3caf8b4eb1c9d023e849624
Kerynn Davis <kerynn.davis@gmail.com>
2023-06-28 12:59:07 -0700

    add search button to dictionary page

--- src/pages/Dictionary.js
+++ src/pages/Dictionary.js
@@ -1,16 +1,9 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Dictionary(){
   const [word, setWord] = useState('');
-  const [word2, setWord2] = useState('');
-  
-  useEffect(() => {
-    console.log('State Updated ' + word);
-  }, [word]); 
-
-  useEffect(() => {
-    console.log('State Updated ' + word2);
-  }, [word2]); 
+  const navigate = useNavigate();
 
   return (
     <>
@@ -20,14 +13,13 @@ export default function Dictionary(){
           setWord(e.target.value);
         }} 
       />
-      <h2>Let's get the definition for {word}</h2>
-      <input 
-        type="text" 
-        onChange={(e) => {
-          setWord2(e.target.value);
-        }} 
-      />
-      <h2>Let's get the definition for {word2}</h2>
+      <button 
+        onClick={() => {
+          navigate('/definition/' + word, {replace: true});
+        }}
+      >
+        Search
+      </button>
     </>
   );
-}
+} 
