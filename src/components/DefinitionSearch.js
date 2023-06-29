cc651119c67a7f70bb65d8d26b44d4a89e1d9b6d
Kerynn Davis <kerynn.davis@gmail.com>
2023-06-29 11:21:33 -0700

    create a definition search component

---
+++ src/components/DefinitionSearch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function DefinitionSearch(){
+  const [word, setWord] = useState('');
+  const navigate = useNavigate();
+
+  return (
+    <form 
+      className="flex justify-center space-x-2 max-w-[300px]"
+      onSubmit={() => {
+      navigate('/definition/' + word)
+    }}>
+      <input
+        className="shrink min-w-0 border border-gray-300 rounded-md p-1"
+        placeholder="Word to Search" 
+        type="text" 
+        onChange={(e) => {
+          setWord(e.target.value);
+        }} 
+      />
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        Search
+      </button>
+    </form>
+  );
+} 
