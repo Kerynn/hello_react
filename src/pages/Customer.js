4b4315965411e391c22abd22617a1ae91ab56774
Kerynn Davis <kerynn.davis@gmail.com>
2023-07-27 14:34:51 -0700

    remove delete button if customer does not exist

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -107,30 +107,30 @@ export default function Customer(){
               </button> 
             </>
             ) : null
-          }
+          } 
+          <button 
+            onClick={(e) => {
+              const url = baseUrl + 'api/customers/' + id;
+              fetch(url, {
+                method: 'DELETE', 
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then((response) => {
+                if(!response.ok){
+                  throw new Error('Something went wrong');
+                }
+                navigate('/customers');
+                }).catch((e) => {
+                  console.log(e);
+                });
+              }}
+          >
+            Delete
+          </button>
         </div> 
       ) : null }
-      <button 
-        onClick={(e) => {
-          const url = baseUrl + 'api/customers/' + id;
-          fetch(url, {
-            method: 'DELETE', 
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          .then((response) => {
-            if(!response.ok){
-              throw new Error('Something went wrong');
-            }
-          navigate('/customers');
-          }).catch((e) => {
-            console.log(e);
-          });
-        }}
-      >
-        Delete
-      </button>
       <br />
       <Link to="/customers">Back to Customers List</Link>
     </>
