315415087ab854de55ee007e0c5762a27943f4a6
Kerynn Davis <kerynn.davis@gmail.com>
2023-07-28 11:31:45 -0700

    update add employee button

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -17,7 +17,7 @@ function AddEmployee(props) {
     <>
       <button 
               onClick={handleShow} 
-              className="block mx-auto m2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              className="block mx-auto m2 bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
             + Add Employee
       </button>
 
